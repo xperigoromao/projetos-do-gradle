a1bbfd7e37fc6e0963bfc3a908ba2139182fd0bc
xperigoromao <romaocarneiro@hotmail.com>
2022-01-12 21:52:40 -0300

    enviando o primeiro comite do projeto

---
+++ encurtador_url-master/src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://paula:paula123@encurtador-url-dio.o9khs.mongodb.net/encurtador-url-dio?retryWrites=true&w=majority',
+}
